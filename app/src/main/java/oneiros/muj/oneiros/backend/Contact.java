d28b5731b20a173c7dcbc7e4c3fdd02b13c98ecf
vidu171 <v.vidu171@gmail.com>
2017-09-12 13:09:13 +0530

    backend added

---
+++ app/src/main/java/oneiros/muj/oneiros/backend/Contact.java
@@ -0,0 +1,24 @@
+package oneiros.muj.oneiros.backend;
+
+/**
+ * Created by Siddharth on 30-08-2017.
+ */
+
+public class Contact {
+    public String Name;
+    public String Number;
+    public Contact(){}
+    Contact(String Name, String Number){
+        this.Name=Name;
+        this.Number=Number;
+    }
+
+//    public String getName() {
+//        return Name;
+//    }
+//
+//    public String getNumber() {
+//        return Number;
+//    }
+}
+//Litmust Events
