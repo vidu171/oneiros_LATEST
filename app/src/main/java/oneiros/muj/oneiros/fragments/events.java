8c16645badab155b91d03eebafa4c29c29fc22a8
aesher9o1 <aashiskumar986@gmail.com>
2017-09-12 14:28:44 +0530

    Events created

--- app/src/main/java/oneiros/muj/oneiros/fragments/events.java
+++ app/src/main/java/oneiros/muj/oneiros/fragments/events.java
@@ -7,6 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import oneiros.muj.oneiros.R;
 
 /**
@@ -15,10 +19,52 @@ import oneiros.muj.oneiros.R;
 
 public class events extends Fragment {
 
+    private SlidingUpPanelLayout mLayout;
+
+    @OnClick(R.id.cini)
+        public void CINIFI(){
+        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+    }
+
+    @OnClick(R.id.shabd)
+        public void SHABD(){
+        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+    }
+
+    @OnClick(R.id.Litmus)
+        public void LITMUS(){
+        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+    }
+    @OnClick(R.id.TMC)
+        public void TMC(){
+        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+    }
+    @OnClick(R.id.scribbles)
+        public void SCRIBBLES(){
+        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+    }
+    @OnClick(R.id.SOPHIA)
+        public void SOPHIA(){
+        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+    }
+    @OnClick(R.id.Aperture)
+        public void Aperture(){
+        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+    }
+
+    @OnClick(R.id.coreoe)
+        public void coreoe(){
+        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_events,container,false);
+        ButterKnife.bind(this,v);
+        mLayout = v.findViewById(R.id.sliding_layout);
         return v;
     }
+
+
 }
