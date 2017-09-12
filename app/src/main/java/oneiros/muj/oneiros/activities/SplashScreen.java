63c5d9fc0c2b01eb3a095602e4628c2ec20159f3
vidu171 <v.vidu171@gmail.com>
2017-09-12 13:37:57 +0530

    splash screen complete

--- app/src/main/java/oneiros/muj/oneiros/activities/SplashScreen.java
+++ app/src/main/java/oneiros/muj/oneiros/activities/SplashScreen.java
@@ -5,12 +5,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+import oneiros.muj.oneiros.Database.EventdbHelper;
 import oneiros.muj.oneiros.R;
-
+import oneiros.muj.oneiros.backend.Event;
+import oneiros.muj.oneiros.backend.RetrivedEvent;
 
 
 /**
@@ -20,8 +32,14 @@ import oneiros.muj.oneiros.R;
 public class SplashScreen extends AppCompatActivity {
 
 
+    private FirebaseDatabase mFirebaseData;
+    private DatabaseReference mMessagesDatabaseReference;
+    private ChildEventListener mChildEventListener;
+    ArrayList<RetrivedEvent> mEvents;
+    boolean successful;
 
-ImageView imageView,transactionimage;
+
+    ImageView imageView,transactionimage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,13 +56,54 @@ ImageView imageView,transactionimage;
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreen.this,MainActivity.class);
-                Bundle bundle = ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this, transactionimage,transactionimage.getTransitionName()).toBundle();
-                startActivity(i,bundle);
+                if(!successful){
 
+                    Log.w("Thread", "Complete and succesfull");
+                    Toast.makeText(SplashScreen.this,"Please Check your connection",Toast.LENGTH_LONG).show();
+                    finish();
+                }
             }
-        }; handler.postDelayed(runnable,1000);
+        }; handler.postDelayed(runnable,5000);
 
+
+        mEvents = new ArrayList<>();
+        mFirebaseData = FirebaseDatabase.getInstance();
+        mMessagesDatabaseReference = mFirebaseData.getReference().child("Events");
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Event EventData  = dataSnapshot.getValue(Event.class);
+
+                String ContactVal = "";
+                if(dataSnapshot.hasChild("Contact")){
+                    DataSnapshot contactSnapshot = dataSnapshot.child("Contact");
+                    for (DataSnapshot Contact : contactSnapshot.getChildren()) {
+                        ContactVal+="Name: "+Contact.child("Name").getValue()+"\n";
+                        ContactVal+="Number: "+Contact.child("Number").getValue()+"\n\n";
+                    }
+                }
+                Log.w("Name-->",EventData.Name);
+                mEvents.add(new RetrivedEvent(EventData.Name,EventData.Details,EventData.Rules,EventData.MinParticipant,EventData.MaxParticipant,EventData.Fees,EventData.FeesMode,EventData.JudgingCriteria,EventData.Duration,EventData.Club,dataSnapshot.getKey(), ContactVal, EventData.Time, EventData.Location));
+                if(mEvents.size()==39) {
+                    EventdbHelper dbHelper = new EventdbHelper(SplashScreen.this);
+                    dbHelper.reset_data_list(mEvents);
+                    successful = true;
+                    Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                    Bundle bundle = ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this, transactionimage,transactionimage.getTransitionName()).toBundle();
+                    startActivity(i, bundle);
+                    finish();
+                }
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+        mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
 
     }
 }
