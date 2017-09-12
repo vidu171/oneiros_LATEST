1d9d130e9493780af36662e26b2eb15ccc8dbbc6
vidu171 <v.vidu171@gmail.com>
2017-09-12 18:09:09 +0530

    Recyclerview Fill complete

--- app/src/main/java/oneiros/muj/oneiros/fragments/events.java
+++ app/src/main/java/oneiros/muj/oneiros/fragments/events.java
@@ -3,11 +3,15 @@ package oneiros.muj.oneiros.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
@@ -16,7 +20,10 @@ import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import oneiros.muj.oneiros.Database.EventdbHelper;
 import oneiros.muj.oneiros.R;
+import oneiros.muj.oneiros.activities.MainActivity;
+import oneiros.muj.oneiros.backend.RetrivedEvent;
 import oneiros.muj.oneiros.backend.recyclerViewAdapter;
 import oneiros.muj.oneiros.backend.recyclerViewProvider;
 
@@ -27,51 +34,17 @@ import oneiros.muj.oneiros.backend.recyclerViewProvider;
 public class events extends Fragment {
 
 
+    static String clubs[]="Aperture#Cinefilia#Coreographia#Litmus#Scribbles#Shabd#Sophia#The Music Club".split("#");
+    static String clubUniqueIds[]="-KtNY1ZLdhpVMvj_sFMx#-KtNY1ZTH2f8oV9miI28#-KtNY1ZTH2f8oV9miI29#-KtNY1ZTH2f8oV9miI2A#-KtNY1ZTH2f8oV9miI2B#-KtNY1ZU0kplfYYgVEOf#-KtNY1ZU0kplfYYgVEOg#-KtNY1ZU0kplfYYgVEOh".split("#");
 
     RecyclerView recyclerView;
-    recyclerViewAdapter Adapter;
-    List<recyclerViewProvider> List;
+    recyclerViewAdapter mAdapter;
+    ArrayList<RetrivedEvent> List;
+    LinearLayout Cinefilia, Aperture, Coreographia, Litmus, Scribbles, Shabd, Sophia, MusicClub ;
 
 
 
     private SlidingUpPanelLayout mLayout;
-
-    @OnClick(R.id.cini)
-        public void CINIFI(){
-        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-
-    }
-
-    @OnClick(R.id.shabd)
-        public void SHABD(){
-        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-    }
-
-    @OnClick(R.id.Litmus)
-        public void LITMUS(){
-        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-    }
-    @OnClick(R.id.TMC)
-        public void TMC(){
-        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-    }
-    @OnClick(R.id.scribbles)
-        public void SCRIBBLES(){
-        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-    }
-    @OnClick(R.id.SOPHIA)
-        public void SOPHIA(){
-        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-    }
-    @OnClick(R.id.Aperture)
-        public void Aperture(){
-        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-    }
-
-    @OnClick(R.id.coreoe)
-        public void coreoe(){
-        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-    }
 
     @Nullable
     @Override
@@ -80,29 +53,114 @@ public class events extends Fragment {
         mLayout = v.findViewById(R.id.sliding_layout);
 
         List = new ArrayList<>();
-        Adapter = new recyclerViewAdapter(getContext(),List);
+        List.add(new RetrivedEvent("adaf","asda","fassa",1,2,3,4,"afaf","sfaf","fafaf","afsf","asfas","fsaf","asfasf"));
+        List.add(new RetrivedEvent("adaf","asda","fassa",1,2,3,4,"afaf","sfaf","fafaf","afsf","asfas","fsaf","asfasf"));
+        List.add(new RetrivedEvent("adaf","asda","fassa",1,2,3,4,"afaf","sfaf","fafaf","afsf","asfas","fsaf","asfasf"));
+        List.add(new RetrivedEvent("adaf","asda","fassa",1,2,3,4,"afaf","sfaf","fafaf","afsf","asfas","fsaf","asfasf"));
+        List.add(new RetrivedEvent("adaf","asda","fassa",1,2,3,4,"afaf","sfaf","fafaf","afsf","asfas","fsaf","asfasf"));
+
+
+        mAdapter = new recyclerViewAdapter(getContext(),List);
         recyclerView = v.findViewById(R.id.RECYCLE);
-        demoVALUES();
-        LinearLayoutManager layoutManager =  new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(Adapter);
-        recyclerView.setNestedScrollingEnabled(false);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+//        recyclerView.setNestedScrollingEnabled(false);
 
         ButterKnife.bind(this,v);
+
+        Cinefilia = (LinearLayout) v.findViewById(R.id.cini);
+        Aperture = (LinearLayout) v.findViewById(R.id.Aperture);
+        Coreographia = (LinearLayout) v.findViewById(R.id.coreoe);
+        Litmus = (LinearLayout) v.findViewById(R.id.Litmus);
+        Scribbles = (LinearLayout) v.findViewById(R.id.scribbles);
+        Shabd = (LinearLayout) v.findViewById(R.id.shabd);
+        Sophia = (LinearLayout) v.findViewById(R.id.SOPHIA);
+        MusicClub = (LinearLayout) v.findViewById(R.id.TMC);
+
+        final EventdbHelper eventdbHelper = new EventdbHelper(getContext());
+        Cinefilia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List = eventdbHelper.getEventList(clubUniqueIds[1]);
+//                mAdapter.notifyDataSetChanged();
+                mAdapter = new recyclerViewAdapter(getContext(),List);
+                recyclerView.setAdapter(mAdapter);
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+            }
+        });
+        Aperture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List = eventdbHelper.getEventList(clubUniqueIds[0]);
+                mAdapter = new recyclerViewAdapter(getContext(),List);
+                recyclerView.setAdapter(mAdapter);
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+            }
+        });
+        Coreographia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List = eventdbHelper.getEventList(clubUniqueIds[2]);
+                mAdapter = new recyclerViewAdapter(getContext(),List);
+                recyclerView.setAdapter(mAdapter);
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+            }
+        });
+        Litmus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List = eventdbHelper.getEventList(clubUniqueIds[3]);
+                mAdapter = new recyclerViewAdapter(getContext(),List);
+                recyclerView.setAdapter(mAdapter);
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+            }
+        });
+        Scribbles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List = eventdbHelper.getEventList(clubUniqueIds[4]);
+                mAdapter = new recyclerViewAdapter(getContext(),List);
+                recyclerView.setAdapter(mAdapter);
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+            }
+        });
+        Shabd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List = eventdbHelper.getEventList(clubUniqueIds[5]);
+                mAdapter = new recyclerViewAdapter(getContext(),List);
+                recyclerView.setAdapter(mAdapter);
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+            }
+        });
+        Sophia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List = eventdbHelper.getEventList(clubUniqueIds[6]);
+                mAdapter = new recyclerViewAdapter(getContext(),List);
+                recyclerView.setAdapter(mAdapter);
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+            }
+        });
+        MusicClub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List = eventdbHelper.getEventList(clubUniqueIds[7]);
+                mAdapter = new recyclerViewAdapter(getContext(),List);
+                recyclerView.setAdapter(mAdapter);
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+
+            }
+        });
+
+
 
         return v;
     }
 
 
-    public void demoVALUES(){
-
-
-        recyclerViewProvider p1 = new recyclerViewProvider("MY NAME IS");
-        List.add(p1);
-
-
-    }
 
 
 }
