1b953e6eb91e505bc22909b31b606a12a3fb557a
aesher9o1 <aashiskumar986@gmail.com>
2017-09-23 00:37:38 +0530

    added Layout inflaters for View and scan qr codes.
    dialogue_scan and view_qr.... working with zxing library currently

--- app/src/main/java/oneiros/muj/oneiros/activities/MainActivity.java
+++ app/src/main/java/oneiros/muj/oneiros/activities/MainActivity.java
@@ -1,8 +1,7 @@
 package oneiros.muj.oneiros.activities;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -10,10 +9,15 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import oneiros.muj.oneiros.R;
 import oneiros.muj.oneiros.backend.pagerAdapter;
@@ -35,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     TabLayout top;
     ViewPager Pagerfragments;
 
+    String Generate_name = "Aashis is a good boy";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,26 +82,37 @@ public class MainActivity extends AppCompatActivity {
     //Todo View Qr code goes here
     public void View_qr(View V){
 
+        home.CLOSE_FAB();
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+
         AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
         LayoutInflater inflater = getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.dialogue_view_qr,null));
-        builder.setCancelable(false);
+        View dialogueView = inflater.inflate(R.layout.dialogue_view_qr,null);
+        builder.setView(dialogueView);
+        ImageView imageView = dialogueView.findViewById(R.id.Qr_view_holder);
 
 
-        builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(getApplicationContext(),"THIS",Toast.LENGTH_SHORT).show();
-            }
-        });
-        builder.setNegativeButton("Cancel",null);
+        try{
+            BitMatrix bitMatrix = multiFormatWriter.encode(Generate_name, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageView.setImageBitmap(bitmap);
+        }
+
+        catch (Exception e){
+            e.printStackTrace();
+        }
 
         builder.create().show();
+
+
+
 
     }
 
     //Todo Scan Qr code goes here
     public void Scan_qr(View v){
+        home.CLOSE_FAB();
         IntentIntegrator integrator = new IntentIntegrator(this);
         integrator.setPrompt("Scan the club's QR code");
         integrator.setOrientationLocked(false);
@@ -110,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (result != null) {
             if (result.getContents() == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+        //        Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             } else {
 
                 Toast.makeText(this, ""+result.getContents()+""+result.getFormatName(), Toast.LENGTH_LONG).show();
