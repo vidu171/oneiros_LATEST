6680ff1a3a55354dcc4edb407f9f02b0454472ed
siddharthjaidka <siddharthjaidka@gmail.com>
2017-09-25 19:01:55 +0530

    Changes dapter

--- app/src/main/java/oneiros/muj/oneiros/Activities/UserCreds.java
+++ app/src/main/java/oneiros/muj/oneiros/Activities/UserCreds.java
@@ -19,6 +19,7 @@ public class UserCreds {
         this.Contact = Contact;
         this.University = University;
     }
+    public UserCreds(){}
 
 }
 
--- app/src/main/java/oneiros/muj/oneiros/Constants.java
+++ app/src/main/java/oneiros/muj/oneiros/Constants.java
@@ -8,6 +8,7 @@ public class Constants {
 
     public static final String EVENTS_TABLE_REFERENCE = "Events";
     public static final String REGISTRATION_TABLE_REFERENCE = "Registration";
+    public static final String USER_TABLE_REFERENCE = "Users";
 
 
 }
