0bf60a9737b8041a9d36319d76424b6215a6de7a
aesher9o1 <aashiskumar986@gmail.com>
2017-09-26 01:29:01 +0530

    bhaiya arrow dal dena please!!

--- app/src/main/java/oneiros/muj/oneiros/Activities/Developers.java
+++ app/src/main/java/oneiros/muj/oneiros/Activities/Developers.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -32,10 +33,28 @@ public class Developers extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.developers);
         frameLayout = findViewById(R.id.background_changer);
-        horizontalInfiniteCycleViewPager = findViewById(R.id.hicvp);
 
+
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                horizontalInfiniteCycleViewPager = findViewById(R.id.hicvp);
+                horizontalInfiniteCycleViewPager.setVisibility(View.INVISIBLE);
                 horizontalInfiniteCycleViewPager.setAdapter(new HorizontalPagerAdapter_developers(getApplicationContext()));
+            }
+        };handler.postDelayed(runnable,37);
 
+
+        Handler handler2 = new Handler();
+        Runnable runnable2 = new Runnable() {
+            @Override
+            public void run() {
+
+                horizontalInfiniteCycleViewPager.setVisibility(View.VISIBLE);
+
+            }
+        };handler2.postDelayed(runnable2,41);
 
     }
 
@@ -50,7 +69,7 @@ public class Developers extends AppCompatActivity {
         String siddhart = "The wizard of Manipal University. He can do magic with his laptop. Need help? Stuck at code? Knock at his place. He'll be keen to help........ Didn't answer? Must be sleeping ;D.";
         String vidyanshu = "CS engineer in making. Loves SpongeBob for no reason. Or maybe there is one. No one would ever know. And yeah Algorithms turns him on!";
         String akhil = "Dashing enthusiast and Dashing. Add one more of that Dashing. Also a survivor... Team Oneiros would know <3 ";
-
+        String hodor = "Being a student of Information Technology, he has a inquisitive personality and a zeal to learn more with inclination towards Android Development and Cyber Security.";
         private final HorizontalPager_utilities.LibraryObject[] LIBRARIES = new HorizontalPager_utilities.LibraryObject[]{
 
                 new HorizontalPager_utilities.LibraryObject(R.drawable.sid, R.drawable.sid_sid,"Siddhart Jaidka","Advisory"
@@ -64,6 +83,8 @@ public class Developers extends AppCompatActivity {
 
                 new HorizontalPager_utilities.LibraryObject(R.drawable.akhil, R.drawable.akhil_akhil,"Akhil Gupta","Organizing Committee"
                         ,akhil),
+                new HorizontalPager_utilities.LibraryObject(R.drawable.hodor, R.drawable.hodor_hodor,"Sanidhya Kumar","Volunteer"
+                        ,hodor),
 
         };
 
@@ -78,7 +99,7 @@ public class Developers extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 4;
+            return 5;
         }
 
         @Override
@@ -113,7 +134,7 @@ public class Developers extends AppCompatActivity {
 
 
     public void SET_BACKGROUND(){
-        int images[]= {R.drawable.sid_sid,R.drawable.nibble_nibble,R.drawable.vidu  ,R.drawable.akhil_akhil};
+        int images[]= {R.drawable.sid_sid,R.drawable.nibble_nibble,R.drawable.vidu  ,R.drawable.akhil_akhil, R.drawable.hodor_hodor};
         Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), images[horizontalInfiniteCycleViewPager.getRealItem()]);
         frameLayout.setScaleType(ImageView.ScaleType.CENTER_CROP);
         Blurry.with(Developers.this).animate(200).radius(13).async().from(largeIcon).into(frameLayout);
