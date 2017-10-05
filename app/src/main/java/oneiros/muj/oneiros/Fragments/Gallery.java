a9155811a3d47891ba98052a981712867ce65cdc
aesher9o1 <aashiskumar986@gmail.com>
2017-10-05 21:32:38 +0530

    All issues Resolved

--- app/src/main/java/oneiros/muj/oneiros/Fragments/Gallery.java
+++ app/src/main/java/oneiros/muj/oneiros/Fragments/Gallery.java
@@ -23,7 +23,7 @@ public class Gallery {
                 new Image(R.drawable.posterfasion),
                 new Image(R.drawable.posterdjn),
                 new Image(R.drawable.posterguest),
-                new Image(R.drawable.posterdest),
-                new Image(R.drawable.posterhardy));
+                new Image(R.drawable.posterdesta),
+                new Image(R.drawable.posterhardya));
     }
 }
